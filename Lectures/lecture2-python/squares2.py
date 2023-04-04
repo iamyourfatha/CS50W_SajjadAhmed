ac027556cd0b8d6a47b3cd47c9315e5f264d034d
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-04 16:58:38 -0400

    move to new Repo
    moving to better organized repo

---
+++ Lectures/lecture2-python/squares2.py
@@ -0,0 +1,10 @@
+import functions
+#this is importing the entire module
+#if you do not specify what you ase importinh
+# from functions import squares
+
+for i in range(10):
+    print(f"The square of {i} is {functions.square(i)}")
+    #then you have to specify it in the function you need it for
+    # functions.square : the file.function that your importing
+
