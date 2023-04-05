35e1ddbef900444d6ee835dfacba723e5da2b644
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-05 18:24:27 -0400

    .

--- djangowork/djangowork/hello/views.py
+++ djangowork/djangowork/hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def sajjad(request):
     return HttpResponse("Hello, Sajjad!")
