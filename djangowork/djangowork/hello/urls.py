92c4c1d66415dc43340f4b3a32c3327da79aec00
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-05 17:59:00 -0400

    .

--- djangowork/djangowork/hello/urls.py
+++ djangowork/djangowork/hello/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views #views.py and urls.py are in the same directory, so we can use "." for the "from"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("sajjad", views.sajjad, name="sajjad"), 
+    path("shah", views.shah, name="shah"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- djangowork/djangowork/hello/views.py
+++ djangowork/djangowork/hello/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!")
+
+def sajjad(request):
+    return HttpResponse("Hello, Sajjad!")
+
+def shah(request):
+    return HttpResponse("Selam, Shah Gi!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
