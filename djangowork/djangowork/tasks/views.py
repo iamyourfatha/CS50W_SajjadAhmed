ae2f4a0d662bda61867bc8a5a9dd6fc426fcc4db
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-07 17:28:22 -0400

    .

--- djangowork/djangowork/tasks/views.py
+++ djangowork/djangowork/tasks/views.py
@@ -5,7 +5,6 @@ tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form): #the capitaliation of the F in Form matttered here
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -15,6 +14,15 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            } )
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
