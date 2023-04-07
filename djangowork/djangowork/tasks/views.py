e8d50a3055775b72eb63433a88662d261e0909eb
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-07 17:42:39 -0400

    sessions

--- djangowork/djangowork/tasks/views.py
+++ djangowork/djangowork/tasks/views.py
@@ -3,15 +3,17 @@ from django.urls import reverse
 from django import forms
 from django.shortcuts import render
 
-tasks = []
 
 class NewTaskForm(forms.Form): #the capitaliation of the F in Form matttered here
     task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks #on the right means its the value the variables takes on, its a python variable
+        "tasks": request.session["tasks"] #on the right means its the value the variables takes on, its a python variable
         #on the left, its the name of the varbialbe that html can access
     })
 
@@ -20,7 +22,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
